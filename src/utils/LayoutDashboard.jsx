3291e8ecc4ebb6c4cc262ab13329a581cdbf2aad
AbanobAshrafX <abanobashraf07@gmail.com>
2025-05-03 22:45:14 +0300

    fix(LayoutDashboard): Correct state variable naming and improve formatting for better readability

--- src/utils/LayoutDashboard.jsx
+++ src/utils/LayoutDashboard.jsx
@@ -5,22 +5,26 @@ import Sidebar from '../components/provider/Sidebar/SidebarProvider';
 import Invite from '../components/provider/Invite/Invite';
 
 const LayoutDashboard = () => {
-  const[IsOpen,setIsOpen]=useState()
-  const handleIsopen =()=> {
-setIsOpen(true)
-    
-  }
-  const handleIsclose=()=>{
-setIsOpen(false)
-  }
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleIsopen = () => {
+    setIsOpen(true);
+  };
+
+  const handleIsclose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <div className="flex h-screen overflow-hidden bg-[#F5F6FA] relative">
-
       <Sidebar className="z-10" handleIsopen={handleIsopen} />
       <div className="flex flex-col flex-1">
-        <Invite isOpen={IsOpen} handleIsclose={handleIsclose} />
-        <Navbar className="z-20"  />
-        <div className="flex-1 lg:h-[83vh] outline-0 rounded-2xl mx-6 bg-[#F5F6FA] font-poppins overflow-y-auto"  style={{ scrollbarWidth: 'thin', scrollbarColor: '#4B5563 ##1E1E1EBF' }}>
+        <Invite isOpen={isOpen} handleIsclose={handleIsclose} />
+        <Navbar className="z-20" />
+        <div
+          className="flex-1 lg:h-[83vh] outline-0 rounded-2xl mx-6 bg-[#F5F6FA] font-poppins overflow-y-auto"
+          style={{ scrollbarWidth: 'thin', scrollbarColor: '#4B5563 #1E1E1EBF' }}
+        >
           <Outlet />
         </div>
       </div>
