6ba5d73aefff3511e720607d8a6a08e181ba00b8
AbanobAshrafX <abanobashraf07@gmail.com>
2025-05-04 22:13:26 +0300

    fix(LayoutDashboard): update import paths for components to reflect new directory structure

--- src/utils/LayoutDashboard.jsx
+++ src/utils/LayoutDashboard.jsx
@@ -1,30 +1,26 @@
 import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from '../components/provider/NavbarProvider';
-import Sidebar from '../components/provider/Sidebar/SidebarProvider';
-import Invite from '../components/provider/Invite/Invite';
+import Navbar from '../components/merchant/NavbarProvider';
+import Sidebar from '../components/merchant/Sidebar/SidebarProvider';
+import Invite from '../components/merchant/Invite/Invite';
 
 const LayoutDashboard = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
+  const [IsOpen, setIsOpen] = useState()
   const handleIsopen = () => {
-    setIsOpen(true);
-  };
+    setIsOpen(true)
 
+  }
   const handleIsclose = () => {
-    setIsOpen(false);
-  };
-
+    setIsOpen(false)
+  }
   return (
     <div className="flex h-screen overflow-hidden bg-[#F5F6FA] relative">
+
       <Sidebar className="z-10" handleIsopen={handleIsopen} />
       <div className="flex flex-col flex-1">
-        <Invite isOpen={isOpen} handleIsclose={handleIsclose} />
+        <Invite isOpen={IsOpen} handleIsclose={handleIsclose} />
         <Navbar className="z-20" />
-        <div
-          className="flex-1 lg:h-[83vh] outline-0 rounded-2xl mx-6 bg-[#F5F6FA] font-poppins overflow-y-auto"
-          style={{ scrollbarWidth: 'thin', scrollbarColor: '#4B5563 #1E1E1EBF' }}
-        >
+        <div className="flex-1 lg:h-[83vh] outline-0 rounded-2xl mx-6 bg-[#F5F6FA] font-poppins overflow-y-auto" style={{ scrollbarWidth: 'thin', scrollbarColor: '#4B5563 ##1E1E1EBF' }}>
           <Outlet />
         </div>
       </div>
