58f6d780c9478c1cb7ac74a17241120a96718381
AbanobAshrafX <abanobashraf07@gmail.com>
2025-05-10 15:51:07 +0300

    refactor: Simplify Vite configuration by removing unused server settings and optimizing Emotion dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,66 +2,32 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin']
+      }
+    })
+  ],
   css: {
     postcss: './postcss.config.js',
   },
-  server: {
-    port: 5000,
-    host: '0.0.0.0',
-    watch: {
-      usePolling: true
-    },
-    proxy: {
-      '/users-service': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      }
-    }
+  optimizeDeps: {
+    include: ['@emotion/react', '@emotion/styled', '@emotion/cache']
   },
   build: {
-    // Optimize chunk size
-    chunkSizeWarningLimit: 1000,
-    // Improve long-term caching with deterministic hashes
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
-          // Remove React from manualChunks since it's external
           vendor: ['react-router-dom'],
           mui: ['@mui/material', '@mui/icons-material'],
-          emotion: ['@emotion/react', '@emotion/styled'],
+          emotion: ['@emotion/react', '@emotion/styled', '@emotion/cache'],
           charts: ['apexcharts', 'react-apexcharts', '@mui/x-charts'],
-        },
-        // Customize asset filenames for better caching
-        assetFileNames: 'assets/[name].[hash].[ext]',
-      },
-      external: []
-    },
-    // Ensure assets are properly hashed for cache busting
-    assetsInlineLimit: 4096, // 4kb - default value
-  },
+        }
+      }
+    }
+  }
 })
-
-
-
-
-
-
-
-
-
-
 
