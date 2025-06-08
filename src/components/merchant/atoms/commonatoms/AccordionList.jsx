98264434bf2c2b815db3e058547a2a6449abdfe5
AbanobAshrafX <abanobashraf07@gmail.com>
2025-06-08 10:14:22 +0300

    feat: refine AccordionList component by removing unnecessary font classes

--- src/components/merchant/atoms/commonatoms/AccordionList.jsx
+++ src/components/merchant/atoms/commonatoms/AccordionList.jsx
@@ -9,21 +9,21 @@ const AccordionList = ({ sections }) => {
   };
 
   return (
-    <div className="p-4  font-poppins">
-      {sections.map((section, index) => (
+    <div className="p-4  ">
+      {sections?.map((section, index) => (
         <div className="space-y-2" key={index}>
           <div
             className="border-b-2 border-gray-200 flex justify-between items-center px-4 py-2 cursor-pointer"
             onClick={() => handleClick(index)}
           >
             <div>
-              <h3 className='text-xl font-poppins text-[#404040] opacity-[50] font-[700] py-2'>{section.title}</h3>
+              <h3 className='text-xl  text-[#404040] opacity-[50] font-[700] py-2'>{section.title}</h3>
             </div>
             <div>{activeIndex === index ? <IoIosArrowUp /> : <IoIosArrowDown />}</div>
           </div>
           {activeIndex === index && (
             <div className="p-4">
-              <h3 className='text-[1rem] opacity-75 font-bold'>{section.content}</h3>
+              <h3 className='text-[1rem] opacity-75'>{section.content}</h3>
             </div>
           )}
         </div>
