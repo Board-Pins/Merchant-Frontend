2629dd15d6f9b9792aa212c371d994f4ec5a1ed0
Abanob Ashraf <57045510+AbanobashrafX@users.noreply.github.com>
2025-06-15 20:59:14 +0300

    refactor(LayoutDashboard): streamline invite and profile modal handling

--- src/utils/LayoutDashboard.jsx
+++ src/utils/LayoutDashboard.jsx
@@ -6,25 +6,44 @@ import Invite from '../components/merchant/Invite/Invite';
 import WelcomeModal from '../components/merchant/WelcomeModal';
 
 const LayoutDashboard = () => {
-  const [IsOpen, setIsOpen] = useState()
-  const handleIsopen = () => {
-    setIsOpen(true)
+  const [isInviteOpen, setIsInviteOpen] = useState(false);
+  const [isProfileModalOpen, setIsProfileModalOpen] = useState(true);
 
-  }
-  const handleIsclose = () => {
-    setIsOpen(false)
-  }
+
+  const handleInviteOpen = () => {
+    setIsInviteOpen(true);
+  };
+
+  const handleInviteClose = () => {
+    setIsInviteOpen(false);
+  };
+
   return (
-    <div className="flex h-screen  overflow-hidden bg-[#F5F6FA] relative">
- <WelcomeModal/>
-      <Sidebar className="z-10" handleIsopen={handleIsopen} />
+    <div className="flex h-screen overflow-hidden bg-[#F5F6FA] relative">
+      <Sidebar className="z-10" handleIsopen={handleInviteOpen} />
       <div className="flex flex-col flex-1">
-        <Invite isOpen={IsOpen} handleIsclose={handleIsclose} />
+        <Invite isOpen={isInviteOpen} handleIsclose={handleInviteClose} />
+
+        {/* Profile creation/pending modal */}
+        {isProfileModalOpen && (
+          <WelcomeModal
+            isOpen={isProfileModalOpen}
+            handleIsClose={isProfileModalOpen => setIsProfileModalOpen(isProfileModalOpen)}
+          />
+        )}
+
         <Navbar className="z-20" />
-        <div className="flex-1 lg:h-[83vh] outline-0 rounded-2xl mx-6 bg-[#F5F6FA] font-poppins overflow-y-auto" style={{ scrollbarWidth: 'thin', scrollbarColor: '#4B5563 ##1E1E1EBF' }}>
+        <div
+          className="flex-1 h-full outline-0 rounded-2xl mx-6 bg-[#ffffff] overflow-y-auto"
+          style={{
+            scrollbarWidth: "thin",
+            scrollbarColor: "#4B5563 #ffffff",
+          }}
+        >
           <Outlet />
         </div>
       </div>
+      {/* <DevInfo /> */}
     </div>
   );
 };
