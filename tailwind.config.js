8a66919ed0fd171e0a9fb170ac0f8a3a1422c531
Abanob Ashraf <57045510+AbanobashrafX@users.noreply.github.com>
2025-07-03 19:54:07 +0300

    feat(tailwind.config): extend theme with custom font families, box shadows, and update primary color structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,27 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'sans-serif': ['sans-serif'],
+        'cairo': ['Cairo', 'sans-serif'],
+        'poppins-thin': ['Poppins', 'sans-serif'],
+      },
+      boxShadow: {
+        'custom': '0px 4px 16px 0px rgba(0, 0, 0, 0.1)',
+        'gray': '0 4px 6px rgba(128, 128, 128, 0.5)',
+        'custom2': ' 0px 0px 9.34px 0px #C3C3C340',
+        'custom3': '0px 4px 16px 0px rgba(0, 0, 0, 0.1)',
+
+      },
+
       colors: {
-        primary: {
-          DEFAULT: '#6161FF',
-          50: '#F0F0FF',
-          100: '#E0E0FF',
-          500: '#6161FF',
-          600: '#5050E6',
-          700: '#4040CC',
-        }
+        primary: '#6161FF',
+        dark: "#26222B"
+      },
+      fontWeight: {
+        light: 100,
       }
     },
   },
-  plugins: [],
 }
